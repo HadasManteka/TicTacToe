b5a42ada53672af762f60db46fa1b9d8301c5cbb
Hadas <hadas716@gmail.com>
2022-12-03 16:53:08 +0200

    change names

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -12,16 +12,16 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     public static int countTaps = 0;
-    boolean gameActive = true;
+    boolean isGameActive = true;
 
     //  1 - X
     //  0 - O
     //  2 - empty
-    int activePlayer = 1;
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int activePlayerCode = 1;
+    int[] boardGameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
     // All the win positions
-    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+    int[][] winningCase = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
                             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
                             {0, 4, 8}, {2, 4, 6}};
 
@@ -33,53 +33,53 @@ public class MainActivity extends AppCompatActivity {
 
     // Every tap in an empty box of the grid
     public void playerTap(View view) {
-        if (gameActive) {
-            ImageView img = (ImageView) view;
+        if (isGameActive) {
+            ImageView playerImg = (ImageView) view;
             ImageView turnImg = findViewById(R.id.imageTurn);
-            int tappedImage = Integer.parseInt(img.getTag().toString());
+            int tappedImage = Integer.parseInt(playerImg.getTag().toString());
 
-            writeTurn(turnImg);
+            displayTurn(turnImg);
 
-            if (gameState[tappedImage] == 2) {
+            if (boardGameState[tappedImage] == 2) {
                 countTaps++;
-                gameState[tappedImage] = activePlayer;
+                boardGameState[tappedImage] = activePlayerCode;
 
-                if (activePlayer == 1) {
-                    img.setImageResource(R.drawable.x);
-                    activePlayer = 0;
+                if (activePlayerCode == 1) {
+                    playerImg.setImageResource(R.drawable.x);
+                    activePlayerCode = 0;
                 } else {
-                    img.setImageResource(R.drawable.o);
-                    activePlayer = 1;
+                    playerImg.setImageResource(R.drawable.o);
+                    activePlayerCode = 1;
                 }
             }
 
             checkWinning(turnImg);
 
             if (countTaps == 9) {
-                gameActive = false;
+                isGameActive = false;
                 Button playAgainButton = findViewById(R.id.play_again_button);
                 playAgainButton.setVisibility(View.VISIBLE);
             }
         }
     }
 
-    public void writeTurn(ImageView img) {
-        img.setImageResource((activePlayer == 1) ? R.drawable.oplay : R.drawable.xplay);
+    public void displayTurn(ImageView img) {
+        img.setImageResource((activePlayerCode == 1) ? R.drawable.oplay : R.drawable.xplay);
     }
 
     public void checkWinning(ImageView img) {
         int flag = 0;
         int winIndex = 0;
 
-        for (int[] winPosition : winPositions) {
-            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
-                gameState[winPosition[1]] == gameState[winPosition[2]] &&
-                gameState[winPosition[0]] != 2) {
+        for (int[] winPosition : winningCase) {
+            if (boardGameState[winPosition[0]] == boardGameState[winPosition[1]] &&
+                boardGameState[winPosition[1]] == boardGameState[winPosition[2]] &&
+                boardGameState[winPosition[0]] != 2) {
 
-                gameActive = false;
+                isGameActive = false;
                 flag = 1;
                 setMark(winIndex);
-                img.setImageResource((gameState[winPosition[0]] == 1) ? R.drawable.xwin : R.drawable.owin);
+                img.setImageResource((boardGameState[winPosition[0]] == 1) ? R.drawable.xwin : R.drawable.owin);
                 Button playAgainButton = findViewById(R.id.play_again_button);
                 playAgainButton.setVisibility(View.VISIBLE);
             }
@@ -133,9 +133,9 @@ public class MainActivity extends AppCompatActivity {
         markImg.setImageResource(mark);
     }
 
-    public void gameReset(View view) {
-        gameActive = true;
-        activePlayer = 1;
+    public void resetGame(View view) {
+        isGameActive = true;
+        activePlayerCode = 1;
         countTaps = 0;
         ImageView markImg = findViewById(R.id.mark);
         markImg.setImageResource(R.drawable.empty);
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView turnImg = findViewById(R.id.imageTurn);
         turnImg.setImageResource(R.drawable.xplay);
 
-        Arrays.fill(gameState, 2);
+        Arrays.fill(boardGameState, 2);
 
         ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
         ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
